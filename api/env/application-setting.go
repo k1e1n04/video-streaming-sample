a6b19f28914c06cfcc32d7113dc0365d51a2b4f2
KenIshii <riguveda44@gmail.com>
2025-03-20 00:25:50 +0900

    add: setting

---
+++ api/env/application-setting.go
@@ -0,0 +1,56 @@
+package env
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+// ApplicationSetting is application setting
+type ApplicationSetting struct {
+	env             string
+	awsRegion       string
+	videoBucketName string
+}
+
+const LocalEnv = "local"
+
+// NewApplicationSetting is generate ApplicationSetting
+func NewApplicationSetting() *ApplicationSetting {
+	if err := godotenv.Load(); err != nil {
+		log.Printf(".env ファイルが存在しませんでした。")
+	}
+	env := os.Getenv("ENV")
+	if env == "" {
+		env = LocalEnv
+	}
+	awsRegion := os.Getenv("AWS_REGION")
+	if awsRegion == "" {
+		awsRegion = "ap-northeast-1"
+	}
+	videoBucketName := os.Getenv("VIDEO_BUCKET_NAME")
+	if videoBucketName == "" {
+		videoBucketName = "local-video-bucket"
+	}
+
+	return &ApplicationSetting{
+		env:             env,
+		awsRegion:       awsRegion,
+		videoBucketName: videoBucketName,
+	}
+}
+
+// Env is get env
+func (a *ApplicationSetting) Env() string {
+	return a.env
+}
+
+// AWSRegion is get aws region
+func (a *ApplicationSetting) AWSRegion() string {
+	return a.awsRegion
+}
+
+// VideoBucketName is get video bucket name
+func (a *ApplicationSetting) VideoBucketName() string {
+	return a.videoBucketName
+}
