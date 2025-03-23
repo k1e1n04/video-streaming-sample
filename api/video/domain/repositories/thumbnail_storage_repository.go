d364a3219ce3e64e690087671900b310836ed97f
KenIshii <riguveda44@gmail.com>
2025-03-23 14:12:54 +0900

    update: storage

--- api/video/domain/repositories/thumbnail_storage_repository.go
+++ api/video/domain/repositories/thumbnail_storage_repository.go
@@ -3,14 +3,12 @@ package repositories
 import (
 	"bytes"
 	"context"
-
 	"github.com/k1e1n04/video-streaming-sample/api/video/domain/entities"
 )
 
-// VideoStorageRepository is a video storage repository
-type VideoStorageRepository interface {
-	// Store is a method to store video
-	Store(ctx context.Context, videoID entities.VideoID, video *bytes.Reader, extension string) error
+type ThumbnailStorageRepository interface {
+	// Store is a method to store thumbnail
+	Store(ctx context.Context, videoID entities.VideoID, thumbnail *bytes.Reader, extension string) error
 	// GetPresignedURLByVideoID is a method to get unsigned URL by video ID
 	GetPresignedURLByVideoID(ctx context.Context, videoID entities.VideoID, extension string) (string, error)
 }
